ffd2d8c129fa0216f5f3ea6e8f24f3868a18fca5
GreenestGoat <fionnanloughlin@gmail.com>
2024-11-16 07:23:19 +0000

    update

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
+import postcss from 'rollup-plugin-postcss';  // Import the PostCSS plugin
 
 export default {
   input: 'src/index.js', // Entry point of your components
@@ -10,21 +11,22 @@ export default {
     {
       file: 'dist/vueframe.cjs.js',
       format: 'cjs',
-      exports: 'named'
+      exports: 'named',
     },
     {
       file: 'dist/vueframe.esm.js',
       format: 'esm',
-      exports: 'named'
-    }
+      exports: 'named',
+    },
   ],
   plugins: [
     vue(), // This processes .vue files
+    postcss(), // This handles the styles in .vue files
     babel({
-      exclude: 'node_modules/**'
+      exclude: 'node_modules/**',
     }),
     resolve(),
     commonjs(),
-    terser() // Minify the output
-  ]
+    terser(), // Minify the output
+  ],
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import { defineAsyncComponent } from 'vue';
+import YouTube from './components/YouTube.vue';
+import Dailymotion from './components/Dailymotion.vue';
+import Vimeo from './components/Vimeo.vue';
 
 export default {
     install(app) {
-        // Use import.meta.glob to load all components dynamically
-        const components = import.meta.glob('./components/*.vue', { eager: true });
-
-        // Iterate over all components and register them globally
-        Object.keys(components).forEach((path) => {
-            const componentName = path.split('/').pop().replace('.vue', ''); // Get component name without extension
-            const component = components[path]; // Import component
-
-            // Register component globally
-            app.component(componentName, defineAsyncComponent(() => Promise.resolve(component)));
-        });
+        // Register each component globally
+        app.component('YouTube', YouTube);
+        app.component('Dailymotion', Dailymotion);
+        app.component('Vimeo', Vimeo);
     },
 };
