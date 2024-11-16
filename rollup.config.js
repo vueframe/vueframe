b9682f0cd3524f2a70e58bad615c492d2a019364
GreenestGoat <fionnanloughlin@gmail.com>
2024-11-16 07:37:05 +0000

    update

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import postcss from 'rollup-plugin-postcss';  // Import the PostCSS plugin
+import typescript from 'rollup-plugin-typescript2';  // Add TypeScript plugin
 
 export default {
   input: 'src/index.js', // Entry point of your components
@@ -18,6 +19,11 @@ export default {
       format: 'esm',
       exports: 'named',
     },
+    {
+      file: 'dist/vueframe.d.ts',  // Output for TypeScript declaration
+      format: 'esm',  // It can be ESM format for .d.ts
+      exports: 'named',
+    },
   ],
   plugins: [
     vue(), // This processes .vue files
@@ -28,5 +34,9 @@ export default {
     resolve(),
     commonjs(),
     terser(), // Minify the output
+    typescript({
+      tsconfig: './tsconfig.json',  // Reference your tsconfig.json file
+      useTsconfigDeclarationDir: true,  // Makes sure the .d.ts file is placed correctly
+    }),
   ],
 };
