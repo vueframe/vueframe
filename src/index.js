a46b0e25e3ac14b3cf007d70cdd229f8f4b4b0e4
GreenestGoat <fionnanloughlin@gmail.com>
2024-11-17 14:40:28 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import YouTube from './components/YouTube.vue'
-import Vimeo from './components/Vimeo.vue'
-import Dailymotion from './components/Dailymotion.vue'
+import YouTube from './components/YouTube.vue';
+import Vimeo from './components/Vimeo.vue';
+import Dailymotion from './components/Dailymotion.vue';
 
-const components = { YouTube, Vimeo, Dailymotion }
+const components = { YouTube, Vimeo, Dailymotion };
 
-export default app => {
+const install = (app) => {
   for (const prop in components) {
     if (components.hasOwnProperty(prop)) {
-      app.component(
-        components[prop].name,
-        components[prop]
-      )
+      app.component(components[prop].name, components[prop]);
     }
   }
-}
+};
+
+export { YouTube, Vimeo, Dailymotion };
+
+export default {
+  install,
+};
